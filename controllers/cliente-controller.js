d22f4f53505efef5863c09729d712f441adfb22c
Oscar <oscarmondragon100@gmail.com>
2017-07-27 14:14:59 -0500

    Se agregaron metodos pra eliminar y editar clientes
    Se implementaron rutas para eliminar y editar clientes

--- controllers/cliente-controller.js
+++ controllers/cliente-controller.js
@@ -16,7 +16,7 @@ class ClienteController {
         }) : errors.http401(req, res, next)
     }
     addCliente(req, res, next) {
-        return (req.session.username) ? res.render('add', {
+        return (req.session.username) ? res.render('addCliente', {
             title: 'Agregar Cliente',
             user: req.session.username
         }) : errors.http401(req, res, next);
@@ -38,37 +38,61 @@ class ClienteController {
         };
         console.log(cliente);
         return (req.session.username) ? ClienteModel.count({
-                _id: cliente._id
-            }, (err, count) => {
-                if (err) throw err;
-                console.log(`Número de Docs: ${count}`);
-                if (count == 0) {
-                    ClienteModel.create(cliente, (err) => {
-                        if (err) throw err;
-                        res.redirect('/clientes')
-                    });
-                } else if (count == 1) {
-                    ClienteModel.findOneAndUpdate({
-                        _id: cliente._id
-                    }, {
-                        name: cliente.name,
-                        address: cliente.address,
-                        ipInternet: cliente.ipInternet,
-                        ipDevice: cliente.ipDevice,
-                        fee: cliente.fee,
-                        base: cliente.base,
-                        mac: cliente.mac,
-                        deviceType: cliente.deviceType,
-                        modality: cliente.modality,
-                        channel: cliente.channel,
-                        startDate: cliente.startDate
-                    }, (err) => {
-                        if (err) throw (err);
-                        res.redirect('/clientes');
-                    });
-                }
-            })
-            : errors.http401(req, res, next);
-        }
+            _id: cliente._id
+        }, (err, count) => {
+            if (err) throw err;
+            console.log(`Número de Docs: ${count}`);
+            if (count == 0) {
+                ClienteModel.create(cliente, (err) => {
+                    if (err) throw err;
+                    res.redirect('/clientes')
+                });
+            } else if (count == 1) {
+                ClienteModel.findOneAndUpdate({
+                    _id: cliente._id
+                }, {
+                    name: cliente.name,
+                    address: cliente.address,
+                    ipInternet: cliente.ipInternet,
+                    ipDevice: cliente.ipDevice,
+                    fee: cliente.fee,
+                    base: cliente.base,
+                    mac: cliente.mac,
+                    deviceType: cliente.deviceType,
+                    modality: cliente.modality,
+                    channel: cliente.channel,
+                    startDate: cliente.startDate
+                }, (err) => {
+                    if (err) throw (err);
+                    res.redirect('/clientes');
+                });
+            }
+        }) : errors.http401(req, res, next);
     }
-    module.exports = ClienteController;
+    editCliente(req, res, next) {
+        let _id = req.params._id;
+        console.log(_id);
+        return (req.session.username) ? ClienteModel.findOne({
+            _id: _id
+        }, (err, docs) => {
+            if (err) throw err;
+            console.log(docs);
+            res.render('editCliente', {
+                title: 'Editar Cliente',
+                user: req.session.username,
+                data: docs
+            });
+        }) : errors.http401(req, res, next);
+    }
+    deleteCliente(req, res, next) {
+        let _id = req.params._id;
+        console.log(_id);
+        return (req.session.username) ? ClienteModel.remove({
+            _id: _id
+        }, (err) => {
+            if (err) throw err;
+            res.redirect('/clientes')
+        }) : errors.http401(req, res, next);
+    }
+}
+module.exports = ClienteController;
---
+++ prueba.js
@@ -0,0 +1,7 @@
+/**
+ * Created by OscarMon on 26/07/2017.
+ */
+var cliente {
+    name: "oscar",
+        ip;"192.168.1.1"
+};
